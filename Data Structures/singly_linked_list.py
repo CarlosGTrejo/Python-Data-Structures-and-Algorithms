068b9889523e0a74f984ab4db0de3da7657ce2f3
Carlos Trejo <guadarramac74@gmail.com>
2020-02-26 17:20:00 -0600

    Finished singly_linked_list * Removed `self.node_ids` for membership testing (using iterative solution for less memory) * made SLL (sinlgy linked list) iterable * added getitem and setitem * added membership operator (__contains__) * added reverse method * added copy method * added swap method * added repr for Node class to make pdb debugging easier

--- Data Structures/singly_linked_list.py
+++ Data Structures/singly_linked_list.py
@@ -1,21 +1,26 @@
 class Node:
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next = next_node
+    __slots__ = ("data", "next")
+
+    def __init__(self, data: object, next: "Node" = None):
+        self.data, self.next = data, next
+
+    def __repr__(self):
+        return repr((self.data))
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.node_ids = [] # keeps track of the nodes that are in the linked list, helpful when adding a node relative to another node, also helpful for determining length
-    
+        self.length = 0
+
     def __len__(self):
-        return len(self.node_ids)
-    
+        return self.length
+
     def __str__(self) -> str:
         """Returns the content of the linked list."""
         current_node = self.head
-        data = () # Using tuple because it uses less memory
+        data = ()  # Using tuple because it uses less memory
         while current_node:
             data += (current_node.data,)
             current_node = current_node.next
@@ -25,73 +30,104 @@ class LinkedList:
     def __repr__(self):
         return str(self)
 
+    def __iter__(self):
+        """Used whenever linked list needs to be iterated (for-loops, unpacking, etc.)
+        Also gets rid of a lot of while-loops that were used in:
+            * __getitem__
+            * __setitem__
+            * insert_at_index
+            * etc."""
+        curr = self.head
+        while curr:
+            yield curr
+            curr = curr.next
+
     def __getitem__(self, key: int) -> Node:
-        """Returns nodes on given key (index), does not support negative indexing smaller than -1 or slices."""
-        if type(key) is slice: raise NotImplementedError("Slices have are not supported")
-        if type(key) is not int: raise TypeError("indices must be integers")
+        """Returns nodes on given key (index).
+        Does not support negative indexing smaller than -1 or slices."""
+        if isinstance(key, slice):
+            raise NotImplementedError("Slices are not supported")
+        if not isinstance(key, int):
+            raise TypeError("indices must be integers")
 
         pre_tests = (
-            (key >= len(self.node_ids)), # index out of max range
-            (key < 0-len(self.node_ids)), # index out of min range
-            (len(self.node_ids) == 0) # linked list is empty
+            (key >= self.length),  # index out of max range
+            (key < 0 - self.length)  # index out of min range
         )
-        
-        if any(pre_tests): raise IndexError("linked list index out of range")
 
-        if key in (0, 0 - len(self.node_ids)): return self.head
-        elif key in (-1, len(self.node_ids) - 1): return self.tail
-        else: # if index is negative...
-            node = self.head.next # Head was returned in first if statement, no need to start at head
-            index = 1-len(self.node_ids) if key < 0 else 1 # Because head was returned, there is also no need to have index start at 0 or the min negative index
+        if any(pre_tests):
+            raise IndexError("linked list index out of range")
+
+        if key in (0, 0 - self.length):
+            return self.head
+        elif key in (-1, self.length - 1):
+            return self.tail
+        else:  # if index is negative...
+            node = self.head.next
+            # Because head was returned, there is no need to
+            # have index start at 0 or the min negative index
+            index = 1 - self.length if key < 0 else 1
             while index != key:
                 node = node.next
                 index += 1
 
-            return node # On index and key match, return node
+            return node  # On index and key match, return node
 
     def __setitem__(self, key, value):
         """Replaces the node's data at index, 'key', with 'value'."""
-        if type(key) is slice: raise TypeError("indices must be integers, not slices")
+        if isinstance(key, slice):
+            raise TypeError("indices must be integers, not slices")
+
         self[key].data = value
-    
+
+    def __contains__(self, item):
+        for node in self:
+            if (node == item) or (node.data == item):
+                return True
+        else:
+            return False
+
     def append(self, data) -> None:
         """Adds (appends) a node to the end of the list."""
         new_node = Node(data)
-        if not self.node_ids: # if the linked list is empty, add the first node
+        if self.length == 0:  # Add first node if list is empty
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next, self.tail = new_node, new_node
 
-        self.node_ids.append(id(new_node))
+        self.length += 1
 
     def prepend(self, data) -> None:
         """Adds a node to the beginning of the list (prepend)."""
-        self.head = Node(data, next_node=self.head)
-        self.node_ids.append(id(self.head))
+        self.head = Node(data, next=self.head)
+        self.length += 1
 
     def append_at_node(self, node: Node, data):
         """Inserts a new node after the given node."""
         if not isinstance(node, Node):
             raise TypeError("Argument 1 is not a node.")
 
-        if id(node) not in self.node_ids:
+        if node not in self:
             raise KeyError("Node not in linked list.")
 
-        node.next = Node(data, next_node=node.next)
-        self.node_ids.append(id(node.next))
+        node.next = Node(data, next=node.next)
+        self.length += 1
 
     def insert_at_index(self, index: int, data) -> None:
-        """Inserts a node at the given index, if the index is out of range, then it appends it to the end."""
-        if type(index) is not int: raise TypeError("index must be an integer.")
-        if index < 0: raise ValueError("index must be non-negative")
-        
+        """Inserts a node at the given index.
+        If the index is out of range, then it appends it to the end."""
+        if not isinstance(index, int):
+            raise TypeError("index must be an integer.")
+        if index < 0:
+            raise ValueError("index must be non-negative")
+
         if index == 0:
             self.prepend(data)
-        elif index >= len(self.node_ids):
+        elif index >= self.length:
             self.append(data)
         else:
-            # Not starting at head, because if index was 0, then we would've already replaced the head in the second if statement.
+            # Not starting at head, 2nd if statment handled it
             node = self.head.next
             last_node = self.head
             node_index = 1
@@ -101,22 +137,132 @@ class LinkedList:
                 node_index += 1
 
             # Node indexes matched, so insert node
-            last_node.next = Node(data,next_node=node)
-            self.node_ids.append(id(last_node.next))
+            last_node.next = Node(data, next=node)
+            self.length += 1
 
     def remove_node(self, data) -> None:
         """Removes a node that matches the given data."""
-        if len(self.node_ids) == 0: raise ValueError(f"Data {data} not in linked list")
+        if self.length == 0:
+            raise ValueError(f"Data {data} not in linked list")
         last_node = cur_node = self.head
         while cur_node.next:
-            if cur_node.data == data: break # break if node data matches given data
+            if cur_node.data == data:
+                break  # break if node data matches given data
             else:
-                last_node = cur_node
-                cur_node = cur_node.next
-        else: # node NOT found
+                last_node, cur_node = cur_node, cur_node.next
+        else:  # node NOT found
             raise ValueError(f"Data {data} not in linked list")
         # node found
         last_node.next = cur_node.next
-        self.node_ids.remove(id(cur_node))
-        cur_node.data = cur_node.next = None # GC will take care of it 
-        
+        cur_node.data = cur_node.next = None  # GC will take care of it
+        self.length -= 1
+
+    def reverse(self):
+        """Reverses the linked list order."""
+        if self.length == 0: return
+
+        curr, last = self.head, None
+        while True:
+            if curr is None: break
+            curr.next, last = last, curr.next
+            if last is None: break
+            last.next, curr = curr, last.next
+        self.tail, self.head = self.head, self.tail
+
+    def copy(self):
+        """Returns a copy of the linked list."""
+        llist_copy = type(self)()
+        for node in self:
+            llist_copy.append(node.data)
+
+        return llist_copy
+
+    def swap(self, key1, key2) -> None:
+        if self.length == 0: raise ValueError("Can't perfrom swap on an empty list")
+        if self.length == 1: raise ValueError("Can't perform swap on a list of length 1")
+
+        if key1 == key2:
+            return
+
+        head_tail = (
+            {self.head, self.tail},
+            {self.head.data, self.tail.data},
+            {self.head.data, self.tail},
+            {self.head, self.tail.data},
+        )
+
+        if {key1, key2} in head_tail:  # if either key1 or key2 is head and the other tail
+            # then swap list head and list tail
+            penultimate = self[-2]  # The node before the tail
+            # swap head tail pointers first
+            self.head, self.tail = self.tail, self.head
+            self.head.next = self.tail.next
+            self.tail.next = None
+            penultimate.next = self.tail
+            return
+
+        prev_1, curr_1 = None, self.head
+        prev_2, curr_2 = None, self.head
+        found_key1, found_key2 = False, False
+
+        for node in self:  # Key searching
+            if key1 in (node, node.data) and not found_key1:  # 1st key found
+                curr_1 = node
+                found_key1 = True
+            elif key2 in (node, node.data) and not found_key2:  # 2nd key found
+                curr_2 = node
+                found_key2 = True
+
+            if found_key1 and found_key2:
+                break  # No need to go through llist in vain if both keys are found
+            else:  # Update prev nodes if keys not found yet
+                prev_1 = node if not found_key1 else prev_1  # Update prev1 if key1 not found
+                prev_2 = node if not found_key2 else prev_2  # Update prev2 if key2 not found
+        else:  # One or more keys was not found
+            if not any((found_key1, found_key2)):  # if neither key was found...
+                raise IndexError(f"{key1} and {key2} are not in linked list.")
+
+            # But if at least 1 key was found...
+            if not found_key1:  # key1 is not in llist, swap key2 for key1
+                if isinstance(key1, Node):  # perform node swap if key1 is Node
+                    if curr_2 == self.head:  # assign new head if key2 is head
+                        self.head = key1
+                    elif curr_2 == self.tail:  # assign new tail if key2 is tail
+                        self.tail = key1
+                        prev_2.next = key1  # prev 2 pointer must be fixed for tail too
+                    else:  # if key2 is not head, fix prev2 pointer
+                        prev_2.next = key1
+                    key1.next = curr_2.next
+                    curr_2.next = None
+                else:  # if key1 is not Node, swap data
+                    curr_2.data = key1
+
+                return
+
+            elif not found_key2:  # key2 is not in llist, swap key1 for key2
+                if isinstance(key2, Node):  # Node swap since key 2 is Node
+                    if curr_1 == self.head:  # assign new head if key1 is head
+                        self.head = key2
+                    elif curr_1 == self.tail:  # assign new tail if key1 is tail
+                        self.tail = key2
+                        prev_1.next = key2  # prev 1 pointer must be fixed for tail
+                    else:
+                        prev_1.next = key2
+                    key2.next = curr_1.next
+                    curr_1.next = None
+                else:  # key2 is not Node, swap key1.data
+                    curr_1.data = key2
+                return
+
+        # Both keys are in llist, but one could be the head
+        if prev_1:
+            prev_1.next = curr_2
+        else:  # Key 1 is head
+            self.head = curr_2
+
+        if prev_2:
+            prev_2.next = curr_1
+        else:  # Key 2 is head
+            self.head = curr_1
+
+        curr_1.next, curr_2.next = curr_2.next, curr_1.next  # swap
--- Data Structures/test_singly_linked_list.py
+++ Data Structures/test_singly_linked_list.py
@@ -1,20 +1,49 @@
+# flake8: noqa
+from itertools import cycle
+
 import pytest
 
 import singly_linked_list
 
+llist = None
 
 def setup_function(function):
     global llist
     llist = singly_linked_list.LinkedList()
 
+
+def test_iter():
+    """Iterating over the list should trigger __iter__ and return Nodes"""
+    for c in 'abc': llist.append(c)
+
+    for letter, node in zip('abc', llist):
+        assert node.data == letter
+
+
+def test_contains():
+    """Doing Node in llist should return True if the Node is in the linked list, False otherwise
+    If it's not a Node, then it should return True if the data/object is in the linked list, False otherwise."""
+    x = singly_linked_list.Node('data')
+    assert x not in llist
+
+    for c in 'abc': llist.append(c)
+
+    assert x not in llist
+    node = llist[1]  # b
+    assert node in llist
+    assert 'a' in llist
+    assert 'z' not in llist
+
+
 def test_append_len():
     """Doing llist.append(obj) should add obj at the end of the linked list."""
     assert len(llist) == 0
     objects = ('a', 1, object(), ['list'], ('tuple',), {0: 'dict'}, {'set'})
-    for length,o in enumerate(objects, start=1):
+    for length, o in enumerate(objects, start=1):
         llist.append(o)
         assert len(llist) == length
         assert llist.tail.data == o
+
 
 def test_str_repr():
     """Calling str() and repr() on a linked list should output the data in a tuple as a string."""
@@ -25,19 +54,20 @@ def test_str_repr():
     assert str(llist) == "('a', 123)"
     assert repr(llist) == "('a', 123)"
 
+
 def test_getitem():
     """Given an integer, n, llist[n] should return the node at position n"""
     with pytest.raises(TypeError):
         llist['a']
-    
+
     with pytest.raises(IndexError):
         llist[0]
-    
+
     with pytest.raises(NotImplementedError):
         llist[:]
-    
+
     for c in 'abcdefg': llist.append(c)
-    
+
     assert llist[0].data == 'a'
     assert llist[1].data == 'b'
 
@@ -45,19 +75,20 @@ def test_getitem():
     assert llist[-3].data == 'e'
     assert len(llist) == len('abcdefg')
 
+
 def test_setitem():
     """llist[n] == obj should insert obj at index n"""
     with pytest.raises(IndexError):
         llist[0] = 'new data'
-    
+
     llist.append('a')
-    
+
     with pytest.raises(TypeError):
         llist['a'] = 'new data'
-    
+
     with pytest.raises(TypeError):
         llist[2:3] = 'new data'
-    
+
     llist[0] = 'new data'
 
     assert len(llist) == 1
@@ -75,52 +106,56 @@ def test_setitem():
     assert len(llist) == 4
     assert str(llist) == "('new data', 'x', 'b', 'tail')"
 
+
 def test_prepend():
     """Doing llist.prepend(obj) should add a new node with data, obj, at the beginning of the linked list."""
     objects = ('a', 1, object(), ['list'], ('tuple',), {0: 'dict'}, {'set'})
-    for length,o in enumerate(objects, start=1):
+    for length, o in enumerate(objects, start=1):
         llist.prepend(o)
         assert len(llist) == length
         assert llist.head.data == o
+
 
 def test_append_at_node():
     """Doing llist.append_at_node(node, obj), should add a new node containing data, obj, after the given node."""
     for o in (1, 'a', [], {}, ()):
         with pytest.raises(TypeError):
             llist.append_at_node(o, 'DATA')
-    
-    outside_node = singly_linked_list.Node('new node data')    
+
+    outside_node = singly_linked_list.Node('new node data')
     with pytest.raises(KeyError):
         llist.append_at_node(outside_node, 'New-er node data')
-    
+
     for c in 'abc':
         llist.append(c)
-    
+
     llist.append_at_node(llist[1], 'X')
     assert str(llist) == "('a', 'b', 'X', 'c')"
+
 
 def test_insert_at_index():
     """Doing llist.insert_at_index(n) should insert a new node at the given index, n."""
     for c in 'abc': llist.append(c)
-    
+
     with pytest.raises(TypeError):
         llist.insert_at_index('index', 'new data')
-    
+
     with pytest.raises(ValueError):
         llist.insert_at_index(-2, 'new data')
-    
+
     llist.insert_at_index(1, 'X')
     assert str(llist) == "('a', 'X', 'b', 'c')"
 
     llist.insert_at_index(400, 'tail')
     assert str(llist) == "('a', 'X', 'b', 'c', 'tail')"
-    
+
     llist.insert_at_index(0, 'head')
     assert str(llist) == "('head', 'a', 'X', 'b', 'c', 'tail')"
 
     llist.insert_at_index(4, '3')
     assert str(llist) == "('head', 'a', 'X', 'b', '3', 'c', 'tail')"
     assert len(llist) == 7
+
 
 def test_remove_node():
     """Doing llist.remove_node(data) should remove the first occurence of the node containing the given data."""
@@ -134,9 +169,153 @@ def test_remove_node():
     assert llist[2] != node_3
     assert len(llist) == len('abccd') - 1
     assert str(llist) == "('a', 'b', 'c', 'd')"
-    
+
     with pytest.raises(ValueError):
         llist.remove_node('data')
+
+
+def test_reverse():
+    """Doing llist.reverse() should reverse the data in the list"""
+    llist.reverse()
+
+    for c in 'abcde':
+        llist.append(c)
+
+    llist.reverse()
+    assert str(llist) == "('e', 'd', 'c', 'b', 'a')", f"List Content: {llist}"
+
+
+def test_copy():
+    """llist.copy() should return an exact copy of the data in the list"""
+    for c in 'abc': llist.append(c)
+
+    l_copy = llist.copy()
+    assert str(l_copy) == str(llist)
+
+
+def test_swap_small():
+    """Doing llist.swap(N1,N2) in a list of length 0 or 1 should return ValueError"""
+    with pytest.raises(ValueError):
+        llist.swap('a', 'b')
+
+    llist.append('a')
+
+    with pytest.raises(ValueError):
+        llist.swap('a','b')
+
+def test_swap():
+    """Doing llist.swap(N1, N2) should swap nodes N1 and N2."""
+    for c in 'abcde': llist.append(c)
+
+    llist.swap('a', 'a')
+    assert str(llist) == str(tuple('abcde'))
+
+    with pytest.raises(IndexError):
+        N1 = singly_linked_list.Node('data1')
+        N2 = 'data2'
+        llist.swap(N1, N2)
+
+    # Head-Tail swap
+    cases = (
+        ('a', 'e'),  # Neither is node
+        (llist.head, llist.tail.data),  # One is node
+        (llist.tail, llist.head),  # Both are nodes
+        (llist.tail.data, llist.head)  # Switching order back to normal
+    )
+    for i,case in enumerate(cases, start=1):
+        llist.swap(*case)
+        assert {llist.head.data, llist.tail.data} == {'e', 'a'}, f"\n  Case #{i}: {case}\n{'='*20}"
+
+        assert sorted((n.data for n in llist)) == list('abcde')
+
+    # list data = a b c d e
+    llist.swap('b', 'c')
+    assert llist[1].data == 'c' and llist[2].data == 'b'
+    assert str(llist) == str(tuple('acbde'))
+
+    llist.swap('c', 'b')
+
+    llist.swap('b', 'd')
+    # a d c b e
+    assert llist[1].data == 'd' and llist[-2].data == 'b'
+    assert str(llist) == str(tuple('adcbe'))
+
+
+    # Swapping by Node content
+    # list data: a d c b e
+    N1 = singly_linked_list.Node('x')
+    llist.swap(N1, 'b')
+    assert llist[-2].data == 'x', f"List Contents: {llist}"
+
+    llist.swap('b', N1)
+    assert llist[-2].data == 'b', f"List Contents: {llist}"
+
+    llist.swap('x', 'b')
+    assert llist[-2].data == 'x', f"List Contents: {llist}"
+
+    llist.swap('b', 'x')
+    assert llist[-2].data == 'b', f"List Contents: {llist}"
+    assert str(llist) == str(tuple('adcbe'))
+
+
+    # Head Swapping
+    N2 = singly_linked_list.Node('H')
+    old_head = llist[0]
+    llist.swap(N2, llist[0])
+    assert llist[0] == N2, f"List Contents: {llist}"
+
+    llist.swap(N2, old_head)
+    assert llist[0] == old_head, f"List Contents: {llist}"
+    # list data: a d c b e
+    assert str(llist) == str(tuple('adcbe'))
+
+
+    # Tail swapping
+    # ++Both keys are nodes++
+    # Key1 is Node and not in list
+    new_tail = singly_linked_list.Node('tail')
+    og_tail = llist.tail
+    llist.swap(new_tail, llist[-1])
+    assert llist[-1].data == new_tail.data, f"List Contents: {llist}"
+
+    llist.swap(llist[-1], og_tail)
+    assert llist[-1] == og_tail, f"List Contents: {llist}"
+    assert str(llist) == str(tuple('adcbe')), "List contents are not correct"
+
+
+    # Key2 is Node and not in list
+    N5 = singly_linked_list.Node('-2')
+    old_node = llist[-2]
+    llist.swap(old_node.data, N5)
+    assert llist[-2] == N5, breakpoint()
+    llist.swap(old_node, N5)
+    assert str(llist) == str(tuple('adcbe'))
+
+
+    # Key2 is NOT Node and not in list
+    old_node = 'b'
+    llist.swap(llist[-2], 'not node')
+    assert llist[-2].data == 'not node'
+
+    llist.swap(llist[-2], old_node)
+    assert str(llist) == str(tuple('adcbe')), f"List Contents: {llist}"
+
+
+    # Both keys are in list
+    # Head swap
+    old_head = llist.head
+    new_head = llist.head.next
+    llist.swap(llist.head, llist.head.next)
+    assert llist.head == new_head
+
+    # contents: d a c b e
+
+    old_node = llist[-2]
+    # breakpoint()
+    llist.swap(old_node, llist.head)
+    assert llist.head == old_node
+
+    assert str(llist) == str(tuple('bacde'))
 
 
 def teardown_function(function):
